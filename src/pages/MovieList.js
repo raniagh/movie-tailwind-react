fb99ff845b20a9bb8b34349f0f88331fc3e54bec
Your Name <rghzaiel@ggn-group.com>
2023-05-29 11:36:52 +0100

    tailwind css customization

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -6,7 +6,7 @@ export const MovieList = ({ apiPath }) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
           {movies &&
             movies.map((movie) => <Card key={movie.id} movie={movie} />)}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        other: {
+          min: "340px",
+          max: "1200px",
+        },
+      },
+      colors: {
+        darkbg: "#1E293B",
+      },
+    },
   },
   plugins: [],
 };
