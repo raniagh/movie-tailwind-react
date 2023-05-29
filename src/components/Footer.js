657cf471bd3b4d8e80ca97e36c4394e7e6781841
Your Name <rghzaiel@ggn-group.com>
2023-05-29 17:26:23 +0100

    updated footer href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,22 +12,22 @@ export const Footer = () => {
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
+          <a href="/" target="_blank" className="mr-4 hover:underline md:mr-6 ">
             Instagram
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+          <a href="/" target="_blank" className="mr-4 hover:underline md:mr-6">
             LinkedIn
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+          <a href="/" target="_blank" className="mr-4 hover:underline md:mr-6">
             Youtube
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="hover:underline">
+          <a href="/" target="_blank" className="hover:underline">
             Github
           </a>
         </li>
